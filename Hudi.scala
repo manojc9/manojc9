5dcd12712e24a8e5f218d8541b2cf6291bb48e7e
MANOJ KUMAR <manojsc99@gmail.com>
2024-09-17 10:19:06 +0530

    Update Hudi.scala

--- Hudi.scala
+++ Hudi.scala
@@ -1,7 +1,12 @@
-import org.apache.hudi.QuickstartUtils._
-import org.apache.hudi.config.HoodieWriteConfig
+import org.apache.spark.sql.SparkSession
 import org.apache.hudi.DataSourceWriteOptions._
-import org.apache.spark.sql.SaveMode
+import org.apache.hudi.config.HoodieWriteConfig
+
+val spark = SparkSession.builder()
+  .appName("HudiWriteExample")
+  .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")  // Set KryoSerializer
+  .config("spark.sql.hive.convertMetastoreParquet", "false")
+  .getOrCreate()
 
 val data = Seq(
   ("1", "John", "Doe"),
@@ -11,8 +16,11 @@ val data = Seq(
 val df = spark.createDataFrame(data).toDF("id", "first_name", "last_name")
 
 df.write.format("hudi")
-  .option("hoodie.datasource.write.recordkey.field", "id")
-  .option("hoodie.datasource.write.precombine.field", "id")
-  .option("hoodie.table.name", "test_hudi_table")
-  .mode(SaveMode.Overwrite)
+  .option(TABLE_TYPE_OPT_KEY, "COPY_ON_WRITE")
+  .option(PRECOMBINE_FIELD_OPT_KEY, "id")
+  .option(RECORDKEY_FIELD_OPT_KEY, "id")
+  .option(PARTITIONPATH_FIELD_OPT_KEY, "")
+  .option(HoodieWriteConfig.TBL_NAME, "test_hudi_table")
+  .mode("overwrite")
   .save("/tmp/hudi_test")
+

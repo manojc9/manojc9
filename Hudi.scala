26bd5e7b57ef16ac23efeb6c9f6da524a276a2e2
MANOJ KUMAR <manojsc99@gmail.com>
2024-09-17 10:31:11 +0530

    Update Hudi.scala

--- Hudi.scala
+++ Hudi.scala
@@ -2,23 +2,19 @@ import org.apache.spark.sql.SparkSession
 import org.apache.hudi.DataSourceWriteOptions._
 import org.apache.spark.sql.SaveMode
 
-// Initialize SparkSession
 val spark = SparkSession.builder()
   .appName("HudiWriteExample")
-  .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")  // Set KryoSerializer
+  .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")  // KryoSerializer
   .config("spark.sql.hive.convertMetastoreParquet", "false")
-  .getOrCreate()  // Make sure to create the SparkSession here
+  .getOrCreate()
 
-// Sample data
 val data = Seq(
   ("1", "John", "Doe"),
   ("2", "Jane", "Smith")
 )
 
-// Now you can create the DataFrame
 val df = spark.createDataFrame(data).toDF("id", "first_name", "last_name")
 
-// Writing the DataFrame to Hudi
 df.write.format("hudi")
   .option("hoodie.table.name", "test_hudi_table")  // Set the Hudi table name
   .option(TABLE_TYPE_OPT_KEY, "COPY_ON_WRITE")
@@ -27,4 +23,3 @@ df.write.format("hudi")
   .option(PARTITIONPATH_FIELD_OPT_KEY, "")
   .mode(SaveMode.Overwrite)
   .save("/tmp/hudi_test")
-
